9bc1cd5351fce148575fedc9b6a16416e965d209
Danil Boldyrev <daswerq123@gmail.com>
2023-07-09 03:04:13 +0300

    fix

--- utils/reinstall_torch.bat
+++ utils/reinstall_torch.bat
@@ -1,13 +1,6 @@
 @echo off
 cd ..
 cd libs
-cd rvc
-call venv/Scripts/activate
-pip uninstall torch torchaudio
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-call venv/Scripts/deactivate
-cd ..
-cd audio_separator
 call venv/Scripts/activate
 pip uninstall torch torchaudio
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
